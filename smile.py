ffeb1288b363537ba49e80842a8b9e8a1b732ba1
Ubuntu <ubuntu@ip-172-31-45-164.us-west-2.compute.internal>
2018-02-08 01:28:48 +0000

    data augmentation

--- smile.py
+++ smile.py
@@ -4,6 +4,7 @@ from keras.layers.core import Dense, Dropout, Flatten, Reshape
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from wandb.wandb_keras import WandbKerasCallback
+from skimage import filters
 import wandb
 import smiledataset
 
@@ -14,6 +15,28 @@ config.epochs=10
 
 # load data
 train_X, train_y, test_X, test_y = smiledataset.load_data()
+
+train_X /= 255.0
+test_X /= 255.0
+
+# data augmentation
+# flip x-y
+flip_train_X = np.array(train_X)
+for img in flip_train_X:
+	img = img[:, ::-1]
+
+np.append(flip_train_X, train_X)
+np.append(train_y, train_y)
+
+# blur
+blur_train_X = np.array(train_X)
+for img in blur_train_X:
+	img = filters.gaussian(img, sigma=16)
+
+np.append(blur_train_X, train_X)
+np.append(train_y, train_y)
+
+# shuffle?
 
 # convert classes to vector
 num_classes = 2
@@ -26,17 +49,20 @@ img_rows, img_cols = train_X.shape[1:]
 test_X = test_X.reshape(test_X.shape[0], test_X.shape[1], test_X.shape[2], 1)
 train_X = train_X.reshape(train_X.shape[0], train_X.shape[1], train_X.shape[2], 1)
 
-train_X /= 255.0
-test_X /= 255.0
-
 model = Sequential()
-model.add(Conv2D(32,
-	(3, 3),
-	input_shape=(img_rows, img_cols,1),
-	activation='relu'))
+
+model.add(Conv2D(32, (3, 3), input_shape=(img_rows, img_cols,1), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#model.add(Conv2D(32, (3, 3), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#model.add(Conv2D(64, (3, 3), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.25))
 model.add(Dense(num_classes, activation='softmax') )
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
